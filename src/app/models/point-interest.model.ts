4079e052f3b59a51cdb8b9386b8e54757f5a3dc4
hilderlanherbert <hilderlan.herbert@gmail.com>
2021-02-09 06:31:40 -0300

    Adding models postions and point-interest

---
+++ src/app/models/point-interest.model.ts
@@ -0,0 +1,7 @@
+export interface PointInterestModel{
+    id: number;
+    nome: string;
+    raio: number;
+    latitude: number;
+    longitude: number;
+}
---
+++ src/app/models/position.model.ts
@@ -0,0 +1,9 @@
+export interface PositionModel {
+    id: number;
+    placa: string;
+    data: Date;
+    velocidade: number;
+    latitude: number;
+    longitude: number;
+    ignicao: boolean;
+}
